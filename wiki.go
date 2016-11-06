9f29a33f5c26396b1ff6f14bd80d94cb5d976381
Szczyra, Jan <jan.szczyra@ista.com>
2016-11-06 23:00:00 +0100

    before save handler

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+    "html/template"
     "io/ioutil"
     "net/http"
 )
@@ -25,15 +25,29 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-  p1.save()
-  p2, _ := loadPage("TestPage")
-  fmt.Println(string(p2.Body))
-}
-
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[len("/view/"):]
   p, _ := loadPage(title)
-  fmt.Fpritnf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+  renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+  t, _ := template.ParseFiles(tmpl + ".html")
+  t.Execute(w, p)
+}
+
+func main() {
+  http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  //http.HandleFunc("/save/", saveHandler)
+  http.ListenAndServe(":8080", nil)
 }
