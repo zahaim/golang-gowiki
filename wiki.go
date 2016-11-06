2b371049ee21d45fa8929e5f9b07f898aefa5277
Szczyra, Jan <jan.szczyra@ista.com>
2016-11-06 22:37:08 +0100

    before rewriting main function to be http Handler

--- wiki.go
+++ wiki.go
@@ -3,4 +3,37 @@ package main
 import (
     "fmt"
     "io/ioutil"
+    "net/http"
 )
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+      return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+  p1.save()
+  p2, _ := loadPage("TestPage")
+  fmt.Println(string(p2.Body))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/view/"):]
+  p, _ := loadPage(title)
+  fmt.Fpritnf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
