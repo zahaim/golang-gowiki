a6e02e8aea96c38ada1d81a2a8b83a5bccb58219
Szczyra, Jan <jan.szczyra@ista.com>
2016-11-12 06:24:33 +0100

    before function literals and closures

--- wiki.go
+++ wiki.go
@@ -5,6 +5,8 @@ import (
     "html/template"
     "io/ioutil"
     "net/http"
+    "regexp"
+    "errors"
 )
 
 type Page struct {
@@ -26,8 +28,11 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -36,20 +41,26 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
+  p, err := loadPage(title)
+  if err != nil {
+      p = &Page{Title: title}
+  }
+  renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  err := p.save()
+  err = p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
@@ -57,17 +68,25 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
   http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-  t, err := template.ParseFiles(tmpl + ".html")
+  err := templates.ExecuteTemplate(w, tmpl+".html", p)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
-  err = t.Execute(w, p)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
   }
+  return m[2], nil //The title is the second subexpression
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
