404fea08af1fb9542faff8d3002b3747557e6654
Szczyra, Jan <jan.szczyra@ista.com>
2016-11-13 21:51:17 +0100

    done

--- wiki.go
+++ wiki.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-    "fmt"
+//    "fmt"
     "html/template"
     "io/ioutil"
     "net/http"
     "regexp"
-    "errors"
 )
 
 type Page struct {
@@ -29,10 +28,6 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
   p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -42,10 +37,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
   p, err := loadPage(title)
   if err != nil {
       p = &Page{Title: title}
@@ -54,18 +45,27 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  err = p.save()
+  err := p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
   http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+      m := validPath.FindStringSubmatch(r.URL.Path)
+      if m == nil {
+          http.NotFound(w, r)
+          return
+      }
+      fn(w, r, m[2])
+  }
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
@@ -78,25 +78,14 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
   }
 }
 
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-  m := validPath.FindStringSubmatch(r.URL.Path)
-  if m == nil {
-    http.NotFound(w, r)
-    return "", errors.New("Invalid Page Title")
-  }
-  return m[2], nil //The title is the second subexpression
-}
-
-func sayHello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, I love you %s!", r.URL.Path[:])
-}
+//func sayHello(w http.ResponseWriter, r *http.Request) {
+//  fmt.Fprintf(w, "Hi there, I love you %s!", r.URL.Path[:])
+//}
 
 func main() {
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  http.HandleFunc("/save/", saveHandler)
-  http.HandleFunc("/", sayHello)
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
+//  http.HandleFunc("/", sayHello)
   http.ListenAndServe(":8080", nil)
 }
